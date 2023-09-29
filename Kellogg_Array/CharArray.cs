f79a978acda9338cf132f925357a3bf4cee66246
zitanicole <zita.kellogg@student.centralia.edu>
2023-09-28 22:01:28 -0700

    Add project files.

---
+++ Kellogg_Array/CharArray.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kellogg_Array
+{
+	internal class CharArray<T>
+	{
+		private List<T> list;
+		private Random random;
+		public CharArray() 
+		{
+			list = new List<T>();
+			random = new Random();
+		}	
+		public void Add(T value)
+		{
+			if (list.Count < 50) 
+			{
+				if (!list.Contains(value))
+				{
+					list.Add(value);
+				}
+			}
+		}
+
+		public void Shuffle()
+		{
+			int n = list.Count;
+			while(n > 1)
+			{
+				n--;
+				int k = random.Next(n+1);	
+				T value = list[k];
+				list[k] = list[n];
+				list[n] = value;
+			}
+		}
+
+		public void showArray()
+		{
+			Shuffle();
+			Console.WriteLine("50 element, alphabetical (unique by case) character array:");
+			foreach (T item in list) 
+			{
+				Console.Write(item + "\n");
+			}
+		}
+	}
+}
---
+++ Kellogg_Array/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+namespace Kellogg_Array
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Stopwatch sw = new Stopwatch();
+			sw.Start();
+			CharArray<char> myArray = new CharArray<char>();
+			for (char c = 'A'; c <= 'Z'; c++) 
+			{
+				myArray.Add(c);
+			}
+			for (char c = 'a'; c <= 'z'; c++)
+			{
+				myArray.Add(c);
+			}
+
+			myArray.showArray();
+			sw.Stop();
+			Console.WriteLine("\nElapsed Time: " + sw.Elapsed);
+		}
+	}
+}
